aaf2612b6935e1dff282058597ced25f8002ad74
Le Quoc Vinh <78015080+lequocvinh15042001@users.noreply.github.com>
2022-09-17 17:41:56 +0700

    validation login signup

---
+++ front-end/src/UI/Input/Input.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { useController } from "react-hook-form";
+import { withErrorBoundary } from "react-error-boundary";
+import classes from './Input.module.scss';
+
+function Input(props) {
+    const {
+        id,
+        control,
+        name,
+        type,
+        error = "",
+        placeholder = "",
+        children,
+        ...rest
+      } = props;
+      const { field } = useController({
+        control,
+        name,
+        defaultValue: "",
+      });
+  return (
+    <div className={classes.container}>
+        <input
+        id={name}
+        type={type}
+        placeholder={error.length <= 0 ? placeholder : ""}
+        {...rest}
+        {...field}
+        />
+    {error.length > 0 && (
+        <span>
+          {error}
+        </span>
+      )}
+      {children && (
+        <span>
+          {children}
+        </span>
+      )}
+    </div>
+  )
+}
+
+Input.propTypes = {
+    name: PropTypes.string,
+    type: PropTypes.string,
+    control: PropTypes.any.isRequired,
+}
+
+export default Input
