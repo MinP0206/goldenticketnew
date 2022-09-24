b9809b48e8941403a1203e07880d090c1c04314a
minhphuong <nhoxkazamadepzai@gmail.com>
2022-09-24 08:36:08 +0700

    merge into develop mongodb

--- Golden Ticket New Back-End/goldenmovieticketnew/src/main/java/com/example/goldenmovieticketnew/secutiry/config/WebSecurityConfig.java
+++ Golden Ticket New Back-End/goldenmovieticketnew/src/main/java/com/example/goldenmovieticketnew/secutiry/config/WebSecurityConfig.java
@@ -74,7 +74,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
 			.authorizeRequests().antMatchers("/api/auth/**").permitAll()
 			.antMatchers("/api/test/**").permitAll()
-			.anyRequest().authenticated();
+				.antMatchers("/swagger-ui.html/**").permitAll()
+			.anyRequest().permitAll();
 
 		http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 	}
