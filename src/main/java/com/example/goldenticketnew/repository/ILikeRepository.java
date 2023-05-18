e500d6c8ef1fa8bfc692281f053a046d2bfe1bd3
Minh Phuong <nhoxkazamadepzai@gmail.com>
2023-05-18 17:43:33 +0700

    fix get all like

--- src/main/java/com/example/goldenticketnew/repository/ILikeRepository.java
+++ src/main/java/com/example/goldenticketnew/repository/ILikeRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 public interface ILikeRepository extends JpaRepository<Like,Long>, JpaSpecificationExecutor<Like> {
     Like findFirstByUserIdAndArticleId(Long userId, Long articleId);
+    @Query(value = "select * from like_user where article_id = ?1 and is_like = 1",nativeQuery = true)
     List<Like> findAllByArticleId(Long articleId, Sort sort);
     @Query(value = "select count(id) from like_user where article_id = ?1 and is_like = 1",nativeQuery = true)
     Long countAllByArticleIdAndIsLike(Long articleId);
