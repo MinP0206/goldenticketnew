b3244d4e9c7d52b4e3b03f7616f5e7b40e2af89a
Minh Phuong <nhoxkazamadepzai@gmail.com>
2023-06-03 10:13:52 +0700

    fix schedule

--- src/main/java/com/example/goldenticketnew/payload/resquest/GetAllScheduleRequest.java
+++ src/main/java/com/example/goldenticketnew/payload/resquest/GetAllScheduleRequest.java
@@ -61,14 +61,8 @@ public class GetAllScheduleRequest {
             }
             if (startDate != null) {
                 LocalDate date = LocalDate.parse(startDate);
-                if(date.equals(LocalDate.now())){
-                    System.out.println("toi day r");
-                    System.out.println(LocalTime.now());
-                    predicates.add(cb.greaterThanOrEqualTo(root.get(Schedule.Fields.dateTime), LocalDateTime.now()));
-                }
-                else{
-                    predicates.add(cb.equal(root.get(Schedule.Fields.startDate), date));
-                }
+                predicates.add(cb.equal(root.get(Schedule.Fields.startDate), date));
+
             }
             if (startTime != null) {
                 predicates.add(cb.equal(root.get(Schedule.Fields.startTime), LocalTime.parse(startTime)));
