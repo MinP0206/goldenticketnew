fa88abb76c2a77a5988a4347ee3dc4bf5277ab7d
Minh Phuong <nhoxkazamadepzai@gmail.com>
2023-06-07 20:29:13 +0700

    add value isWaiting

--- src/main/java/com/example/goldenticketnew/dtos/ScheduleDto.java
+++ src/main/java/com/example/goldenticketnew/dtos/ScheduleDto.java
@@ -4,6 +4,7 @@ import com.example.goldenticketnew.model.Schedule;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.TimeZone;
 
@@ -17,6 +18,8 @@ public class ScheduleDto {
     private MovieDto movie;
     private Double price;
 
+    private boolean isWaiting = true;
+
     public ScheduleDto(Schedule schedule) {
         this.id = schedule.getId();
         this.startDate = schedule.getStartDate();
@@ -28,5 +31,14 @@ public class ScheduleDto {
         this.room = new RoomDto(schedule.getRoom());
         this.movie = new MovieDto(schedule.getMovie());
         this.price = schedule.getPrice();
+        LocalDateTime localDateTime = LocalDateTime.now().minusHours(1);
+        System.out.println("Gio hien tai -1 la " + localDateTime);
+        System.out.println("gio chieu cua phim la" + schedule.getDateTime());
+        if(schedule.getDateTime()!=null){
+            System.out.println("gio chieu cua phim la" + schedule.getDateTime());
+            if(schedule.getDateTime().isAfter(localDateTime)){
+                this.isWaiting = false;
+            }
+        }
     }
 }
